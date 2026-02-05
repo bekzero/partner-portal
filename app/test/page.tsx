32f688870353abf7dea84e2d705b599cdee53e40
Ben Eakin <b.eakin@kzero.com>
2026-02-05 17:40:00 -0500

    Add test page to debug 404

---
+++ app/test/page.tsx
@@ -0,0 +1,8 @@
+export default function TestPage() {
+    return (
+        <div className="p-10 text-white bg-black min-h-screen">
+            <h1>Test Page Working</h1>
+            <p>If you can see this, the deployment is serving content.</p>
+        </div>
+    );
+}
